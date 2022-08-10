2be610e447091c519b60fde3a4b93e2478314477
piyushjhawar <piyushjhawar24@gmail.com>
2022-08-09 20:57:54 -0500

    Initial Commit. Implement method based and normal serializers.

---
+++ news/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from news.api.views import article_list_create_api_view, article_detail_api_view
+
+
+urlpatterns = [
+    path("articles/", view=article_list_create_api_view, name="article-list"),
+    path("articles/<int:pk>/", view=article_detail_api_view, name="article-detail"),
+]
