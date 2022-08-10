8cfb382b70035d874938d9e4e552dfbf5393cae0
piyushjhawar <piyushjhawar24@gmail.com>
2022-08-09 21:49:01 -0500

    Implemented all methods with APIView Class.

--- news/api/urls.py
+++ news/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from news.api.views import article_list_create_api_view, article_detail_api_view
-
+from news.api.views import ArticleListCreateAPIView, ArticleDetailAPIView
+# article_list_create_api_view, article_detail_api_view,
 
 urlpatterns = [
-    path("articles/", view=article_list_create_api_view, name="article-list"),
-    path("articles/<int:pk>/", view=article_detail_api_view, name="article-detail"),
+    # path("articles/", view=article_list_create_api_view, name="article-list"),
+    # path("articles/<int:pk>/", view=article_detail_api_view, name="article-detail"),
+    path("articles/", view=ArticleListCreateAPIView.as_view(), name="article-list"),
+    path("articles/<int:pk>/", view=ArticleDetailAPIView.as_view(),
+         name="article-detail"),
 ]
